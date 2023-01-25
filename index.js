513101ab0efac39bc1f833381960ed689a3ab248
josh-willy91 <jwilly91812@yahoo.com>
2023-01-25 11:50:00 -0600

    still working lambda

--- index.js
+++ index.js
@@ -7,29 +7,31 @@ dotenv.config();
 
 
 
-const athenaQueryExecuter = async() => {
-// module.exports.athenaQueryExecuter = async() => {
+// const athenaQueryExecuter = async() => {
+module.exports.athenaQueryExecuter = async() => {
+    console.log('=====Start Function=====')
     const DATABASE = process.env.DATABASE; 
     const QUERY = process.env.QUERY;
     const OUTPUT = process.env.OUTPUT;
+    const OUTPUT_BUCKET = process.env.OUTPUT_BUCKET;
 
-    // console.log('=====s3 client=====', s3Client)
 
-    const bucketParams = {
-        Bucket: "athena-contact-lens-results",
+    const listBucketParams = {
+        Bucket: OUTPUT_BUCKET,
         Prefix: "combinedData"
     };
 
+
     try {
         // list objects in S3 location "athena-contact-lens-results/combinedData"
-        const response = await s3Client.listObjects(bucketParams).promise();
+        const response = await s3Client.listObjects(listBucketParams).promise();
         console.log('=====list bucket=====', response)
 
         // Loop through objects listed and delete each one
         if(response.Contents) {
             response.Contents.forEach(async(file) => {
                 const deleteParams = {
-                    Bucket: "athena-contact-lens-results",
+                    Bucket: OUTPUT_BUCKET,
                     Key: file.Key
                 };
                 
@@ -49,25 +51,12 @@ const athenaQueryExecuter = async() => {
     }
 
 
-    // try {
-    //     const response = await s3.listObjects(bucketParams).promise();
-    //     console.log('=====list bucket=====', response)
-    // } catch(error) {
-    //     console.log('=====list bucket error=====', error)
-    // }
-
     const athenaParams = {
         QueryString: QUERY,
         QueryExecutionContext: {'Database': DATABASE},
         ResultConfiguration: {'OutputLocation': OUTPUT}
     }
 
-    // try {
-    //     const response = await athenaClient.startQueryExecution(athenaParams).promise();
-    //     console.log('=====athena response=====', response)
-    // } catch(error) {
-    //     console.log('=====error=====', error)
-    // }
 
     try {
         const response = await athenaClient.startQueryExecution(athenaParams).promise();
@@ -77,6 +66,7 @@ const athenaQueryExecuter = async() => {
     }
 
 
+    console.log('=====End Function=====')
     return
 }
-athenaQueryExecuter();
+// athenaQueryExecuter();
